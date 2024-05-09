7cdd9fd7e65524345bfdb4491dfc10136a822f42
Ledermayer <18106157+Ledermayer@users.noreply.github.com>
2024-05-09 03:11:47 +0200

    puff

--- terraform/compartments/main.tf
+++ terraform/compartments/main.tf
@@ -2,17 +2,19 @@
 data "oci_identity_compartment" "parent" {
   id = "HU_compartment"
 }
+output "parent" {
+  value = data.oci_identity_compartment.parent
+}
 
 #cmp-root-openlz
-resource "oci_identity_compartment" "test_compartment" {
-  #Required
-  compartment_id = data.oci_identity_compartment.parent.id
-  description    = "Intermediate Root Compartment for the OpenLZ Project"
-  name           = "cmp-root-openlz"
+# resource "oci_identity_compartment" "root" {
+#   compartment_id = data.oci_identity_compartment.parent.id
+#   description    = "Intermediate Root Compartment for the OpenLZ Project"
+#   name           = "cmp-root-openlz"
 
-  freeform_tags = {
-    "Team"      = "Industry Cloud"
-    "Project"   = "OpenLZ"
-    "Terraform" = "true"
-  }
-}
+#   freeform_tags = {
+#     "Team"      = "Industry Cloud"
+#     "Project"   = "OpenLZ"
+#     "Terraform" = "true"
+#   }
+# }
