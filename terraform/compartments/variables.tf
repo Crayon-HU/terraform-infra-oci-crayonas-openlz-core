f4ac0f0c2816dffae5434de5581401e2cc9c02d9
Ledermayer <18106157+Ledermayer@users.noreply.github.com>
2024-05-09 12:50:19 +0200

    skip

--- terraform/compartments/variables.tf
+++ terraform/compartments/variables.tf
@@ -5,6 +5,7 @@ variable "USER_OCID" {
   sensitive = true
 }
 variable "PRIVATE_KEY" {
+  #checkov:skip=CKV_SECRET_13: "Private Key sourced from env vars via action secret."
   sensitive = true
 }
 variable "FINGERPRINT" {
