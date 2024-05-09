342b5e3e69fdeec0afbd499af7a20a5123cff0e0
Ledermayer <18106157+Ledermayer@users.noreply.github.com>
2024-05-09 03:39:26 +0200

    core + open

--- terraform/compartments/main.tf
+++ terraform/compartments/main.tf
@@ -12,9 +12,64 @@ resource "oci_identity_compartment" "root" {
   description    = "Intermediate Root Compartment for the OpenLZ Project"
   name           = "cmp-root-openlz"
 
-  freeform_tags = {
-    "Team"      = "Industry Cloud"
-    "Project"   = "OpenLZ"
-    "Terraform" = "true"
+  freeform_tags = local.tags
+}
+
+#Core Compartments
+locals {
+  enable_delete = false
+  core_compartments = {
+    security = {
+      name        = "cmp-security"
+      description = "Security Compartment for the OpenLZ Project"
+      enable_delete = local.enable_delete
+      tags = local.tags
+    }
+    hub = {
+      name        = "cmp-hub"
+      description = "Hub Compartment for the OpenLZ Project"
+      enable_delete = local.enable_delete
+      tags = local.tags
+    }
+    mgmt = {
+      name        = "cmp-mgmt"
+      description = "Management Compartment for the OpenLZ Project"
+      enable_delete = local.enable_delete
+      tags = local.tags
+    }
   }
+
+  #Operating Entity Compartments
+  open_compartments = {
+    open01 = {
+      name        = "cmp-open01"
+      description = "Operating Entity 01 Compartment for the OpenLZ Project"
+      enable_delete = local.enable_delete
+      tags = local.tags
+    }
+    open02 = {
+      name        = "cmp-open02"
+      description = "Operating Entity 02 Compartment for the OpenLZ Project"
+      enable_delete = local.enable_delete
+      tags = local.tags
+    }
+    open03 = {
+      name        = "cmp-open03"
+      description = "Operating Entity 03 Compartment for the OpenLZ Project"
+      enable_delete = local.enable_delete
+      tags = local.tags
+    }
+  }
+
+  compartments = merge(local.core_compartments, local.open_compartments)
+}
+
+resource "oci_identity_compartment" "core" {
+  for_each       = local.compartments
+  compartment_id = oci_identity_compartment.root.id
+  description    = each.value.description
+  name           = each.value.name
+  enable_delete  = each.value.enable_delete
+
+  freeform_tags = each.value.tags
 }
