4024fa5c55ad4e937a2c4b5723d26d8f9b5c1ba0
Ledermayer <18106157+Ledermayer@users.noreply.github.com>
2024-05-09 04:04:09 +0200

    push

--- terraform/security/auth.tf
+++ terraform/security/auth.tf
@@ -34,5 +34,9 @@ data "terraform_remote_state" "cmp" {
     endpoints = {
       s3 = "https://ax7yjiuzhoio.compat.objectstorage.eu-amsterdam-1.oraclecloud.com"
     }
+    skip_region_validation = true
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
+    force_path_style = true
   }
 }
