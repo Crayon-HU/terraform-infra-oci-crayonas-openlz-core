ef83282111dbb5916686475f6acc1295b9e183a4
Ledermayer <18106157+Ledermayer@users.noreply.github.com>
2024-05-27 11:37:48 +0200

    empty

--- terraform/network/locals.tf
+++ terraform/network/locals.tf
@@ -5,5 +5,5 @@ locals {
     "Terraform" = "true"
   }
   root_compartment_id = "ocid1.tenancy.oc1..aaaaaaaagt7ufsqo7ydlyrzeke6rzxllqtqlhayvhrnoi4vhtyxtq3deo7ia"
-  mgmt_compartment_id = data.terraform_remote_state.cmp.outputs.core_compartments.cmp_mgmt.compartment_id
+  # mgmt_compartment_id = data.terraform_remote_state.cmp.outputs.core_compartments.cmp_mgmt.compartment_id
 }
