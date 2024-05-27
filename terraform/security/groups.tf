b9fd995ffe88cfbf0bd9c70de0be93f709ac6cd0
Ledermayer <18106157+Ledermayer@users.noreply.github.com>
2024-05-27 14:53:53 +0200

    no remote state

--- terraform/security/groups.tf
+++ terraform/security/groups.tf
@@ -1,7 +1,7 @@
 #Security Groups
-data "oci_identity_groups" "root" {
-    compartment_id = data.terraform_remote_state.cmp.outputs.root_compartment_id
-}
+# data "oci_identity_groups" "root" {
+#     compartment_id = data.terraform_remote_state.cmp.outputs.root_compartment_id
+# }
 # output "root_groups" {
 #     value = data.oci_identity_groups.root
 # }
