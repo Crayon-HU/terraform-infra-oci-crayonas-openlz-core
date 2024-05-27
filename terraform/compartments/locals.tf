6d272e5fdc23a07e835c303a064000e95997b12e
PeterB <peterb@noreply.com>
2024-05-27 13:37:07 +0200

    removed tf content

--- terraform/compartments/locals.tf
+++ terraform/compartments/locals.tf
@@ -1,8 +1,8 @@
-locals {
-  tags = {
-    "Team"      = "Industry Cloud"
-    "Project"   = "OpenLZ"
-    "Terraform" = "true"
-  }
-  root_compartment_id = "ocid1.tenancy.oc1..aaaaaaaagt7ufsqo7ydlyrzeke6rzxllqtqlhayvhrnoi4vhtyxtq3deo7ia"
-}
+# locals {
+#   tags = {
+#     "Team"      = "Industry Cloud"
+#     "Project"   = "OpenLZ"
+#     "Terraform" = "true"
+#   }
+#   root_compartment_id = "ocid1.tenancy.oc1..aaaaaaaagt7ufsqo7ydlyrzeke6rzxllqtqlhayvhrnoi4vhtyxtq3deo7ia"
+# }
--- terraform/network/locals.tf
+++ terraform/network/locals.tf
@@ -1,9 +1,9 @@
-locals {
-  tags = {
-    "Team"      = "Industry Cloud"
-    "Project"   = "OpenLZ"
-    "Terraform" = "true"
-  }
-  root_compartment_id = "ocid1.tenancy.oc1..aaaaaaaagt7ufsqo7ydlyrzeke6rzxllqtqlhayvhrnoi4vhtyxtq3deo7ia"
-  # mgmt_compartment_id = data.terraform_remote_state.cmp.outputs.core_compartments.cmp_mgmt.compartment_id
-}
+# locals {
+#   tags = {
+#     "Team"      = "Industry Cloud"
+#     "Project"   = "OpenLZ"
+#     "Terraform" = "true"
+#   }
+#   root_compartment_id = "ocid1.tenancy.oc1..aaaaaaaagt7ufsqo7ydlyrzeke6rzxllqtqlhayvhrnoi4vhtyxtq3deo7ia"
+#   # mgmt_compartment_id = data.terraform_remote_state.cmp.outputs.core_compartments.cmp_mgmt.compartment_id
+# }
