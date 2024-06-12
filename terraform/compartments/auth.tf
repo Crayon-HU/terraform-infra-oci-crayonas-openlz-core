c4dd0dc038fdef1fc39965b9e48e3a2bb17dc19a
PeterB <peterb@noreply.com>
2024-06-12 14:47:09 +0200

    modified compartments to use s3

--- terraform/compartments/auth.tf
+++ terraform/compartments/auth.tf
@@ -15,8 +15,16 @@ terraform {
       version = "5.38.0"
     }
   }
-  backend "pg" {
-    conn_str = "postgres://pxe2wx6joe2zfcn7xdtad5hyqydi5q-primary.postgresql.eu-amsterdam-1.oc1.oraclecloud.com/core" #New
-    # conn_str = "postgres://rn47x2kesus3kbbiylxgrjsl6dzdwq-primary.postgresql.eu-amsterdam-1.oc1.oraclecloud.com/core" #Old
+  # backend "pg" {
+  #   conn_str = "postgres://pxe2wx6joe2zfcn7xdtad5hyqydi5q-primary.postgresql.eu-amsterdam-1.oc1.oraclecloud.com/core" #New
+  #   # conn_str = "postgres://rn47x2kesus3kbbiylxgrjsl6dzdwq-primary.postgresql.eu-amsterdam-1.oc1.oraclecloud.com/core" #Old
+  # }
+  backend "s3" {
+    bucket = "tf-state-bucket-bweoam"
+    region = "eu-amsterdam-1"
+    endpoint = "https://ax7yjiuzhoio.compat.objectstorage.eu-amsterdam-1.oraclecloud.com"
+    dynamodb_endpoint = "https://cbwpnkclljkw26rjvgi3kjdmoe.apigateway.eu-amsterdam-1.oci.customer-oci.com"
+    key = "compartments.tfstate"
+    dynamodb_table = "core"
   }
 }
