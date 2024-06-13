6035160b092f5fe27abdbf8734ffcbed7477365c
PeterB <peterb@noreply.com>
2024-06-13 11:35:00 +0200

    trying to engage locking

--- terraform/compartments/compartments.tf
+++ terraform/compartments/compartments.tf
@@ -64,7 +64,7 @@ locals {
     }
     open05 = {
       name          = "cmp-open05"
-      description   = "Operating Entity 05 Compartment for the OpenLZ Project change2"
+      description   = "Operating Entity 05 Compartment for the OpenLZ Project change3"
       enable_delete = local.enable_delete
       tags          = local.tags
     }
