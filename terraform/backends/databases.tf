f05b57b2f788f7bed24f991da076f3ec33fa4442
PeterB <peterb@noreply.com>
2024-06-27 17:33:02 +0200

    enable backends state

--- terraform/backends/databases.tf
+++ terraform/backends/databases.tf
@@ -19,3 +19,21 @@ resource "postgresql_grant" "mgmt_grant" {
   object_type = "database"
   privileges  = ["ALL"]
 }
+
+resource "postgresql_database" "hub_db" {
+  name = "hub"
+}
+
+resource "postgresql_role" "hub_role" {
+  name     = "hub"
+  login    = true
+  password = "mypass"
+}
+
+# Grant priviliges on the database
+resource "postgresql_grant" "hub_grant" {
+  database    = postgresql_database.hub_db.name
+  role        = postgresql_role.hub_role.name
+  object_type = "database"
+  privileges  = ["ALL"]
+}
