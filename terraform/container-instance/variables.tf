895f188c04deffef4e485cbfbc471c6af4a430d9
PeterB <peterb@noreply.com>
2024-07-15 17:33:59 +0200

    fixin ci bugs

--- terraform/container-instance/variables.tf
+++ terraform/container-instance/variables.tf
@@ -13,6 +13,13 @@ variable "FINGERPRINT" {
   sensitive = true
 }
 
+variable "AWS_ACCESS_KEY_ID" {
+  sensitive = true
+}
+variable "AWS_SECRET_ACCESS_KEY" {
+  sensitive = true
+}
+
 variable "project_name" {
   default = "scylladb"
 }
