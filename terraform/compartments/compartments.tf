b3e6ff5dea9874e35b8ae169f387ac5c0f995a55
PeterB <peterb@noreply.com>
2024-07-15 11:03:52 +0200

    using ci backend

--- terraform/compartments/compartments.tf
+++ terraform/compartments/compartments.tf
@@ -64,7 +64,7 @@ locals {
     }
     open05 = {
       name          = "cmp-open05"
-      description   = "Operating Entity 05 Compartment for the OpenLZ Project change10"
+      description   = "Operating Entity 05 Compartment for the OpenLZ Project change11"
       enable_delete = local.enable_delete
       tags          = local.tags
     }
