0c3957b53306f4237660df64a8a9f4a3d6ddf39c
PeterB <peterb@noreply.com>
2024-07-15 10:41:33 +0200

    using ci backend

--- terraform/compartments/auth.tf
+++ terraform/compartments/auth.tf
@@ -27,7 +27,7 @@ terraform {
 
     endpoints = {
       s3 = "https://ax7yjiuzhoio.compat.objectstorage.eu-amsterdam-1.oraclecloud.com"
-      dynamodb = "https://10.0.1.86:8000"
+      dynamodb = "http://10.0.1.86:8000"
     }
     key = "compartments.tfstate"
     dynamodb_table = "states-ci86"
