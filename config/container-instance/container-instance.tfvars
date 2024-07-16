43170eb5dec374f0b3e17cb3f98d202405d109a7
PeterB <peterb@noreply.com>
2024-07-16 09:25:33 +0200

    modified config

--- config/container-instance/container-instance.tfvars
+++ config/container-instance/container-instance.tfvars
@@ -1,1 +1,4 @@
 private_ip = "10.0.1.77"
+project_name = "scylladb"
+subnet_ocid = "ocid1.subnet.oc1.eu-amsterdam-1.aaaaaaaavxq2fo4ilyaxe5s5oi4smteruiirekbftefn44ua265wg2famcta"
+compartment_ocid = "ocid1.compartment.oc1..aaaaaaaavjic26327yzpleqiu77qvfaxxjobtsuili265urpkh7ac63afo3q"
--- terraform/container-instance/variables.tf
+++ terraform/container-instance/variables.tf
@@ -13,38 +13,7 @@ variable "FINGERPRINT" {
   sensitive = true
 }
 
-variable "project_name" {
-  default = "scylladb"
-}
-variable "subnet_ocid" {
-  default = "ocid1.subnet.oc1.eu-amsterdam-1.aaaaaaaavxq2fo4ilyaxe5s5oi4smteruiirekbftefn44ua265wg2famcta"
-}
+variable "project_name" {}
+variable "subnet_ocid" {}
+variable "compartment_ocid" {}
 variable "private_ip" {}
-
-# variable "containers" {
-#     type = list(object({
-#         display_name          = optional(string)
-#         image_url             = string
-#         environment_variables = optional(map(string))
-
-#         command               = optional(list(string))
-#         arguments             = optional(list(string))
-
-#         volume_mounts         = optional(list(object({
-#             volume_name = string
-#             mount_path  = string
-#         })))
-
-#         resource_config = optional(map(object({
-#             memory_limit_in_gbs = optional(number)
-#             vcpus_limit         = optional(number)
-#         })))
-
-#         memory_limit_in_gbs   = optional(number)
-#         vcpus_limit           = optional(number)
-
-#         working_directory     = optional(string)
-#     }))
-
-#     description = "The containers to create on this container instance."
-# }
